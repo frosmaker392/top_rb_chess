51a29821f478f3e3b4a2d2a9a8865d3dce593cad
zhafir <zzzhafir75@gmail.com>
2020-08-13 14:06:31 +0800

    Add @has_moved variable in ChessPiece

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,12 +1,14 @@
 class ChessPiece
   attr_reader :notation
   attr_reader :side       # White = 1, Black = 2 (cos white moves first for some reason)
+  attr_reader :has_moved
 
   def initialize(notation, side)
     raise "Invalid datatypes!" unless notation.is_a?(String) && side.is_a?(Integer)
 
     @notation = notation
     @side = side
+    @has_moved = false
   end
 
   def debug_str
