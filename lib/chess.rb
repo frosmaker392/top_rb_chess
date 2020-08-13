372e53e3d49534c726759a044124af42cef9d81b
zhafir <zzzhafir75@gmail.com>
2020-08-13 16:09:42 +0800

    Add #move_piece and @captured in ChessData

--- lib/chess.rb
+++ lib/chess.rb
@@ -17,14 +17,36 @@ class ChessPiece
 end
 
 class ChessData
-  attr_reader :grid
+  attr_reader :grid         # To access correctly : @grid[y][x] / @grid[col][row]
+  attr_reader :captured
 
   def initialize
     @grid = Array.new(8) { Array.new(8) {nil} }
+    @captured = []
+
     generate_default_grid
   end
 
   public
+
+  # Moves a chess piece to another point without rules, ignores if out of grid 
+  # or there is nothing on the 'from' position
+  def move_piece(from, to)
+    x_f = from[0]
+    y_f = from[1]
+    x_t = to[0]
+    y_t = to[1]
+
+    return if x_f > 7 || x_f < 0 || y_f > 7 || y_f < 0
+    return if x_t > 7 || x_t < 0 || y_t > 7 || y_t < 0
+    return if @grid[y_f][x_f].nil?
+    
+    unless @grid[y_t][x_t].nil?
+      @captured << @grid[y_t][x_t]
+    end
+    @grid[y_t][x_t] = @grid[y_f][x_f]
+    @grid[y_f][x_f] = nil
+  end
 
   def debug_str
     out = ""
