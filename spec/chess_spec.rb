e7e490d7a5229dac263518ebbe5ac9d16cae54c6
zhafir <zzzhafir75@gmail.com>
2020-08-13 12:03:47 +0800

    Rewrite test for ChessData to only expect a starting chess grid

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -15,20 +15,8 @@ end
 
 describe ChessData do
   context "Initialise ChessData class" do
-    it "initalises ChessData class with an empty 8x8 grid of nils" do
+    it "initialises ChessData class with starting chess pieces" do
       cd = ChessData.new
-      expect(cd.debug_str).to eql("                \n"\
-                                  "                \n"\
-                                  "                \n"\
-                                  "                \n"\
-                                  "                \n"\
-                                  "                \n"\
-                                  "                \n"\
-                                  "                \n")
-    end
-
-    it "initialises ChessData class with @default_grid = true (grid has starting chess pieces)" do
-      cd = ChessData.new(true)
       expect(cd.debug_str).to eql("R2N2B2Q2K2B2N2R2\n"\
                                   "P2P2P2P2P2P2P2P2\n"\
                                   "                \n"\
